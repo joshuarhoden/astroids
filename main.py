13628badfc17ab5e3e02aba627e342fc0f44f142
Joshua Rhoden <jrhoden1313@gmail.com>
2025-02-15 20:10:37 -0500

    Added frame rate restrictions

--- main.py
+++ main.py
@@ -7,16 +7,19 @@ from constants import *
 
 def main():
     pygame.init()
-    print("Starting asteroids!")
-    print(f"Screen width: {constants.SCREEN_WIDTH}")
-    print(f"Screen height: {constants.SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0,0,0))
         pygame.display.flip()
+
+        #limit framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
